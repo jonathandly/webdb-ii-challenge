0a9c1ab7fd1b21f70f97ac5b8743e32d8e084798
Jonathan Daly <dalyj138@gmail.com>
2019-05-25 13:42:34 -0400

    Added bears table and created CRUD capabilities for the stretch problem.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// /api/zoos endpoints here
 server.get('/api/zoos', (req, res) => {
   db('zoos')
     .then(animals => {
@@ -87,6 +87,89 @@ server.put('/api/zoos/:id', (req, res) => {
 
 server.delete('/api/zoos/:id', (req, res) => {
   db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if(count > 0) {
+        res.status(200).json({ message: `${count} record deleted` });
+      } else {
+        res.status(404).json({ message: 'Record doesn\'t exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// /api/bears endpoints
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(animals => {
+      res.status(200).json(animals);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  db('bears')
+    .where({ id: req.params.id })
+    .first()
+    .then(bear => {
+      if(bear) {
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ message: 'Bear not found' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/api/bears', (req, res) => {
+  db('bears')
+    .insert(req.body)
+    .then(results => {
+      return db('bears')
+      .where({ id: results[0] })
+      .first()
+      .then(bear => {
+        if(!req.body) {
+          res.status(400).json({ error: 'Name required' });
+        } else {
+          res.status(201).json(bear.id);
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  db('bears')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(name => {
+      if(!name) {
+        res.status(404).json({ message: 'Record doesn\'t exist' });
+      } else {
+        res.status(200).json({ message: 'Record updated' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  db('bears')
     .where({ id: req.params.id })
     .del()
     .then(count => {
